3a7c621d0210a97bfcb835a61c60361a5038bb00
mrfairbunkle <charliebhfw@icloud.com>
2024-03-19 10:14:52 +0000

    lots of cards and stuff
    Co-authored-by: Charlie Roberts <charlierobertsuk@users.noreply.github.com>

--- Inscryption.py
+++ Inscryption.py
@@ -1,1 +1,51 @@
+import cards, pygame, sys, random, time
 
+# screen size
+screen_width = 1000
+screen_height = 500
+half_width = screen_width // 2
+half_height = screen_height // 2
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# title
+pygame.display.set_caption("Inscryption")
+
+# clock
+clock = pygame.time.Clock()
+
+# initiate pygame
+pygame.init()
+
+# font
+font = pygame.font.Font(None, 36)
+
+# state colours
+KYLE = ("#987654")
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+print(cards.proboscisMonkey["health"])
+
+# constants
+
+# variables
+
+# game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # background colour
+    screen.fill(KYLE)
+
+    
+    # display update
+    pygame.display.update()
+
+    # set game fps
+    clock.tick(60)
