54e218fada20f2b7023b9ebf060219f73198ec52
Charlie Roberts <charlie@robertsclan.co.uk>
2024-03-19 22:18:02 +0000

    Removed testing

--- Network.py
+++ Network.py
@@ -1,19 +1,23 @@
 import socket
 
-class network:
+class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = "192.168.0.114" # cmd - ipconfig - ipv4 - NOTE: needs to be same as server.py address
         self.port = 5555 # also needs to be the same
         self.addr = (self.server, self.port)
         self.id = self.connect()
-        print (self.id)
 
-        def connect(self):
-            try:
-                self.client.connect(self.addr)
-                return self.client.recv(2046).decode()
-            except:
-                pass
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2046).decode()
+        except:
+            pass
 
-n = network()
+    def send(self, data):
+        try:
+            self.client.send(str.encode(data))
+            return self.client.recv(2048).decode()
+        except socket.error as e:
+            print(e)
